6d1c6e847bb96248e0f453cb9b9d791b273ce343
SunProSystemsKe <sunprosystems21@gmail.com>
2025-06-27 12:34:37 +0300

    update

--- BlazorApp7/Components/Test/AnalysisService.cs
+++ BlazorApp7/Components/Test/AnalysisService.cs
@@ -6,13 +6,13 @@ namespace BlazorApp7.Components.Test
 {
     public class AnalysisService
     {
-        string Table(string dbcode) => "sp_analysi" + dbcode;
-        public async Task<IEnumerable<AnalysisMdl>> GetAll(string dbcode)
+        string Table(string dbcode) => "sp_analysi" + dbcode; 
+        public async Task<IEnumerable<AnalysisMdl>> GetAll(string dbcode, string field1, string field2)
         {
-            string[] arr = ["0001", "0002", "0003"];
+            string[] arr = ["0001", "0002", "0003"]; 
             var param = new DynamicParameters();
             param.Add("@ancodes", arr.ToList());
-            string sql = "SELECT * FROM " + Table(dbcode) + " WHERE ancode in @ancodes;";
+            string sql = "SELECT " + field1 + "," + field2 + " FROM " + Table(dbcode) + " WHERE ancode in @ancodes;";
             using (var conn = new MySqlConnection("Server=localhost;Port=3310;Userid=root;Password=SUNPRO100#;Database=SUNPRODB;Sslmode=none;"))
                 return await conn.QueryAsync<AnalysisMdl>(sql, param);
         }
