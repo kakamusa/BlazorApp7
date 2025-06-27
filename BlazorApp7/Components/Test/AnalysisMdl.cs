73e2571b3f671dac8bba2f8c6a0fe5052130d620
SunProSystemsKe <sunprosystems21@gmail.com>
2025-06-27 12:17:30 +0300

    Add project files.

---
+++ BlazorApp7/Components/Test/AnalysisMdl.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp7.Components.Test
+{
+    public class AnalysisMdl
+    {
+        public string Ancode { get; set; }
+        public string Andsc { get; set; }
+    }
+}
---
+++ BlazorApp7/Components/Test/AnalysisService.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Reflection.Emit;
+
+namespace BlazorApp7.Components.Test
+{
+    public class AnalysisService
+    {
+        public async Task<IEnumerable<AnalysisMdl>> GetAll()
+        {
+            string[] arr = ["0001", "0002", "0003"];
+            var param = new DynamicParameters();
+            param.Add("@ancodes", arr.ToList());
+            string sql = "SELECT * FROM sp_analysis WHERE ancode in @ancodes;";
+            using (var conn = new MySqlConnection("Server=localhost;Port=3310;Userid=root;Password=SUNPRO100#;Database=SUNPRODB;Sslmode=none;"))
+                return await conn.QueryAsync<AnalysisMdl>(sql, param);
+        }
+    }
+}
